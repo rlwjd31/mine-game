570425fe47236d5769dd44e0e62b6af15f66fc75
rlwjd31 <rlwjd31@naver.com>
2025-02-17 04:49:14 +0900

    🚀 [Feat]: Add generate custom level of mine
    custom에서 행, 열, 지뢰개수에 따른 동적 board생성에 대한 로직 구현

--- src/constants/level.ts
+++ src/constants/level.ts
@@ -2,4 +2,5 @@ export const levelConfig = {
   beginner: { row: 8, col: 8, numsOfMine: 10 },
   intermediate: { row: 16, col: 16, numsOfMine: 40 },
   expert: { row: 16, col: 32, numsOfMine: 100 },
+  custom: { row: null, col: null, numsOfMine: null },
 };
--- src/store/mine.slice.ts
+++ src/store/mine.slice.ts
@@ -1,22 +1,37 @@
 import { levelConfig } from "@/constants/level";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export type Level = "beginner" | "intermediate" | "expert";
+export type Level = "beginner" | "intermediate" | "expert" | "custom";
+export type LevelConfig = {
+  levelType: Level;
+  row?: number;
+  col?: number;
+  numsOfMine?: number;
+};
 
 export type Mine = `${number},${number}`;
 
 type MinefieldState = {
-  level: Level;
-  mines: Set<`${number},${number}`>
+  levelConfig: LevelConfig;
+  mines: Set<`${number},${number}`>;
 };
 
-const generateMines = (levelType: Level): Set<`${number},${number}`> => {
-  const { row, col, numsOfMine } = levelConfig[levelType];
+const generateMines = ({
+  levelType,
+  row: rowParam,
+  col: colParam,
+  numsOfMine: numsOfMineParam,
+}: LevelConfig): Set<`${number},${number}`> => {
+  const [row, col, numsOfMine] = [
+    rowParam ?? levelConfig[levelType].row,
+    colParam ?? levelConfig[levelType].col,
+    numsOfMineParam ?? levelConfig[levelType].numsOfMine,
+  ];
   const mines = new Set<Mine>();
 
-  while (mines.size < numsOfMine) {
-    const x = Math.floor(Math.random() * row);
-    const y = Math.floor(Math.random() * col);
+  while (mines.size < (numsOfMine ?? 0)) {
+    const x = Math.floor(Math.random() * (row ?? 0));
+    const y = Math.floor(Math.random() * (col ?? 0));
     mines.add(`${x},${y}`); // 중복 방지
   }
 
@@ -24,20 +39,20 @@ const generateMines = (levelType: Level): Set<`${number},${number}`> => {
 };
 
 const initialState: MinefieldState = {
-  level: "beginner",
-  mines: generateMines("beginner"),
+  levelConfig: { levelType: "beginner" },
+  mines: generateMines({ levelType: "beginner" }),
 };
 
 const minefieldSlice = createSlice({
   name: "minefield",
   initialState,
   reducers: {
-    setLevel(state, action: PayloadAction<Level>) {
-      state.level = action.payload;
-      state.mines = generateMines(action.payload);
+    setLevel(state, action: PayloadAction<LevelConfig>) {
+      state.levelConfig = action.payload;
+      state.mines = generateMines({ ...action.payload });
     },
     regenerateMines(state) {
-      state.mines = generateMines(state.level);
+      state.mines = generateMines(state.levelConfig);
     },
   },
 });
