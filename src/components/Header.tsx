e3a6a609894b386bafe3b2a44939d00087b37eb9
rlwjd31 <rlwjd31@naver.com>
2025-02-17 07:01:31 +0900

    🚀 [Feat]: Add custom level input form UI & logic
    custom level을 조정할 수 있게 행, 열, 지뢰 개수를 받을 수 있는 UI 구현과 이에 따른 지뢰 보드 생성 로직 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { levelConfig } from "@/constants/mineInfo";
-import { generateMineBoard } from "@/store/mine.slice";
+import { generateMineBoard, LevelConfig } from "@/store/mine.slice";
 import { RootDispatch } from "@/store/store";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 
 export default function Header() {
   const [isClicked, setIsClicked] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
 
   return (
-    <div className="flex w-full flex-col gap-4 bg-purple-primary text-white">
+    <div className="relative flex w-full flex-col gap-4 bg-purple-primary text-white">
+      {isModalOpen && (
+        <InputModal setModalOpen={setModalOpen} setIsClicked={setIsClicked} />
+      )}
       <div className="relative">
         <button
           className="cursor-pointer text-xl"
@@ -16,7 +20,7 @@ export default function Header() {
         >
           Game
         </button>
-        {isClicked && <Dropdown />}
+        {isClicked && <Dropdown setModalOpen={setModalOpen} />}
       </div>
       <hr className="border-2 border-neutral-200" />
       <div className="flex w-full items-center justify-between text-3xl">
@@ -28,7 +32,7 @@ export default function Header() {
   );
 }
 
-function Dropdown() {
+function Dropdown({ setModalOpen }: { setModalOpen: (prev: boolean) => void }) {
   const dispatch = useDispatch<RootDispatch>();
 
   return (
@@ -72,7 +76,95 @@ function Dropdown() {
       >
         Expert
       </button>
-      <button className="px-4 py-2">Custom</button>
+      <button className="px-4 py-2" onClick={() => setModalOpen(true)}>
+        Custom
+      </button>
+    </div>
+  );
+}
+
+function InputModal({
+  setModalOpen,
+  setIsClicked,
+}: {
+  setModalOpen: (prev: boolean) => void;
+  setIsClicked: (prev: boolean) => void;
+}) {
+  const [levelInfo, setLevelInfo] = useState<LevelConfig>({
+    levelType: "custom",
+    row: 0,
+    col: 0,
+    numsOfMine: 0,
+  });
+  const dispatch = useDispatch<RootDispatch>();
+
+  const defaultStyle = "bg-purple-secondary text-purple-primary";
+  const onClickgenerateMineBoard = () => {
+    dispatch(generateMineBoard({ ...levelInfo }));
+
+    setModalOpen(false);
+    setIsClicked(false);
+  };
+
+  return (
+    <div className="absolute left-0 top-1/2 z-[100] flex w-full flex-col justify-center gap-4 overflow-hidden rounded-xl bg-purple-secondary px-4 py-5 shadow-xl">
+      <button
+        className="w-fit cursor-pointer text-start text-2xl font-bold text-purple-primary hover:bg-purple-primary/30 hover:text-white"
+        onClick={() => setModalOpen(false)}
+      >
+        X
+      </button>
+      <div className="flex gap-4 text-purple-primary">
+        <span>행 입력:</span>
+        <input
+          className={defaultStyle}
+          type="text"
+          placeholder="행 입력"
+          value={levelInfo.row}
+          onChange={(e) =>
+            setLevelInfo((prev) => ({
+              ...prev,
+              row: +e.target.value,
+            }))
+          }
+        />
+      </div>
+      <div className="flex gap-4 text-purple-primary">
+        <span>열 입력:</span>
+        <input
+          className={defaultStyle}
+          type="text"
+          placeholder="열 입력"
+          value={levelInfo.col}
+          onChange={(e) =>
+            setLevelInfo((prev) => ({
+              ...prev,
+              col: +e.target.value,
+            }))
+          }
+        />
+      </div>
+      <div className="flex gap-4 text-purple-primary">
+        <span>지뢰 개수 입력:</span>
+        <input
+          className={defaultStyle}
+          type="text"
+          placeholder="지뢰 개수"
+          value={levelInfo.numsOfMine}
+          onChange={(e) =>
+            setLevelInfo((prev) => ({
+              ...prev,
+              numsOfMine: +e.target.value,
+            }))
+          }
+        />
+      </div>
+      <button
+        className="w-fit cursor-pointer rounded-md bg-purple-primary px-4 py-2 text-white hover:bg-purple-primary/80"
+        onClick={onClickgenerateMineBoard}
+      >
+        지뢰 생성
+      </button>
     </div>
   );
 }
