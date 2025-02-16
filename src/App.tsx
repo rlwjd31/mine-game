59f82477d96298a8b60b12ae88b0b765e8774b43
rlwjd31 <rlwjd31@naver.com>
2025-02-17 00:19:29 +0900

    🚀 [Feat]: Render random generated mine logic
    랜덤으로 생성된 지뢰위치를 board에 표시

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import Header from "@/components/Header";
 import Icon from "@/components/Icon";
 import Layout from "@/components/Layout";
 import MineBody from "@/components/MineBody";
+import { store } from "@/store/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <Layout>
-      <Header />
-      <MineBody />
-      <Icon type="flag" />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Header />
+        <MineBody />
+        <Icon type="flag" />
+      </Layout>
+    </Provider>
   );
 }
 
--- src/components/MineBody.tsx
+++ src/components/MineBody.tsx
@@ -1,25 +1,60 @@
+import { levelConfig } from "@/constants/level";
+import { Level, setLevel } from "@/store/mine.slice";
+import { RootDispatch, RootState } from "@/store/store";
+import { useDispatch, useSelector } from "react-redux";
+
 export default function MineBody() {
   const board = Array.from({ length: 16 }, () =>
     Array.from({ length: 16 }, () => 0)
   );
+  // const {} = useSelector((state: Root))
+  const dispatch = useDispatch<RootDispatch>();
+  const { level: currentLevel, mines } = useSelector(
+    (state: RootState) => state.minefield
+  );
 
+  const handleLevelChange = (level: Level) => {
+    dispatch(setLevel(level));
+  };
+
+  const [ROW, COL] = [
+    levelConfig[currentLevel].row,
+    levelConfig[currentLevel].col,
+  ];
+
+  if (Array.isArray(mines) && mines.length === 0) {
+    return <div>난이도를 설정해주세요</div>;
+  }
+
+  console.log(mines);
   return (
     <div className="flex h-full items-center justify-center">
+      <button onClick={() => handleLevelChange("intermediate")}>
+        지뢰 생성
+      </button>
       <div
-        className="grid size-fit  border border-black "
+        className="grid size-fit border border-black "
         style={{
-          gridTemplateColumns: `repeat(${board[0].length}, 3rem)`, // 열 개수와 크기 설정
-          gridTemplateRows: `repeat(${board.length}, 3rem)`, // 행 개수와 크기 설정
+          gridTemplateRows: `repeat(${levelConfig[currentLevel].row}, 3rem)`, // 행 개수와 크기 설정
+          gridTemplateColumns: `repeat(${levelConfig[currentLevel].col}, 3rem)`, // 열 개수와 크기 설정
         }}
       >
-        {board.map((row, rowIndex) =>
-          row.map((_, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-              className="flex items-center justify-center"
-            >{`[${rowIndex},${colIndex}]`}</div>
-          ))
-        )}
+        {Array(ROW)
+          .fill("")
+          .map((row, rowIndex) =>
+            Array(COL)
+              .fill("")
+              .map((_, colIndex) => (
+                <div
+                  key={`${rowIndex}-${colIndex}`}
+                  className="flex items-center justify-center"
+                >
+                  {mines.has(`${rowIndex},${colIndex}`)
+                    ? "지뢰"
+                    : `${rowIndex},${colIndex}`}
+                </div>
+              ))
+          )}
       </div>
     </div>
   );
